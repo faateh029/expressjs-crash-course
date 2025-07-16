e021b70fa77ceaa45080366561844b3ad5aa4bad
faateh029 <buttfatih691@gmail.com>
2025-07-16 18:25:50 +0500

    created passed html files using  express server response

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express= require('express');
+const path = require('path');
 const app = express();
-
-
-
 app.get('/',(req,res)=>{ 
-    res.send(`<h1>Hello From Express Response hi hi </h1>`)
+    res.sendFile(path.join(__dirname , 'public' , 'index.html'))
 })
-
+app.get('/about' , (req,res)=>{
+    
+    res.sendFile(path.join(__dirname , 'public' , 'about.html'))
+})
 app.listen(5000 , ()=>{console.log('server listening on port 5000')});
