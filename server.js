84319741fd7668c09e61140d56d3f8aa370b54ad
faateh029 <buttfatih691@gmail.com>
2025-07-18 21:14:31 +0500

    created a general error middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from'path';
+import notFound from './Middleware/notFound.js';
 import errorHandler from './Middleware/error.js';
 import posts from './Routes/posts.js'
 import logger from './Middleware/logger.js';
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(logger);
 app.use('/api/posts' , posts);
+app.use(notFound);
 app.use(errorHandler);
 app.listen(port , ()=>{console.log('server listening on port 5000')});
 
