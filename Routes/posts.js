99271e63733521409e03a139cff1064bbf5bfb40
faateh029 <buttfatih691@gmail.com>
2025-07-18 10:29:29 +0500

    Added delete request

--- Routes/posts.js
+++ Routes/posts.js
@@ -52,6 +52,22 @@ router.put('/:id', (req, res) => {
     post.title=req.body.title;
     res.status(200).json(posts);
 })
+
+router.delete('/:id', (req,res)=>{
+    const id = parseInt(req.params.id);
+    const post= posts.find(post=>post.id===id);
+    if(!post){
+        return res
+        .status(400)
+        .json({msg:"enter a valid id"})
+        
+    }
+    posts=posts.filter(post=>post.id!==id);
+    res
+    .status(200)
+    .json(posts);
+    
+})
 //  router.post('/', (req, res) => {
 
 //     // console.log('DEBUG req.body:', req.body); // Add this line
